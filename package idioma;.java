14d56f57551d1bc7203911044febdb2d1e889d5f
Lacielton Brito <oxebrito@gmail.com>
2023-07-04 22:33:05 -0400

    Idioma

---
+++ package idioma;.java
@@ -0,0 +1,16 @@
+package idioma;
+
+import java.util.Locale;
+ 
+        
+public class Idioma {
+
+    
+    public static void main(String[] args) {
+        Locale Idioma = Locale.getDefault();
+
+        System.out.println("A máquina está no idioma: ");
+        System.out.println(Idioma);     
+    }
+    
+}
